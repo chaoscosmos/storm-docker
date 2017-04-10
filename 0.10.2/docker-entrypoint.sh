78c33fef406206460b4d60510e6179e3bbe89337
root <root@eshchao-71.dev>
2017-04-10 11:01:51 +0800

    Remove "-" from the "su" command to pass envirionment

--- 0.10.2/docker-entrypoint.sh
+++ 0.10.2/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
     chown -R "$STORM_USER" "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
 #    exec su-exec "$STORM_USER" "$0" "$@"
     ARGS="$@"
-    exec su "-" "$STORM_USER" "--command=$0 $ARGS"
+    exec su "$STORM_USER" "--command=$0 $ARGS"
 fi
 
 # Generate the config only if it doesn't exist
--- 0.9.4/docker-entrypoint.sh
+++ 0.9.4/docker-entrypoint.sh
@@ -7,18 +7,20 @@ if [ "$1" = 'storm' -a "$(id -u)" = '0' ]; then
     chown -R "$STORM_USER" "$STORM_CONF_DIR" "$STORM_DATA_DIR" "$STORM_LOG_DIR"
 #    exec su-exec "$STORM_USER" "$0" "$@"
     ARGS="$@"
-    exec su "-" "$STORM_USER" "--command=$0 $ARGS"
+    exec su "$STORM_USER" "--command=$0 $ARGS"
 fi
 
+echo $STORM_CONF_DIR
 # Generate the config only if it doesn't exist
-CONFIG="$STORM_CONF_DIR/storm.yaml"
-if [ ! -f "$CONFIG" ]; then
-    cat << EOF > "$CONFIG"
-storm.zookeeper.servers: [zookeeper]
-nimbus.seeds: [nimbus]
-storm.log.dir: "$STORM_LOG_DIR"
-storm.local.dir: "$STORM_DATA_DIR"
-EOF
-fi
+#CONFIG="$STORM_CONF_DIR/storm.yaml"
+#if [ ! -f "$CONFIG" ]; then
+#    touch $CONFIG && chmod 777 $CONFIG
+#    cat << EOF > "$CONFIG"
+#storm.zookeeper.servers: [zookeeper]
+#nimbus.seeds: [nimbus]
+#storm.log.dir: "$STORM_LOG_DIR"
+#storm.local.dir: "$STORM_DATA_DIR"
+#EOF
+#fi
 
-exec "$@"
+#exec "$@"
